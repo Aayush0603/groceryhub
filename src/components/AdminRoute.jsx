104c5c669513ec5d0a1972298d5ce6cbafa97429
Aayush <aayushdoshi126@gmail.com>
2026-05-26 14:54:31 +0530

    Shifted whatsapp to site Ordering

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -1,7 +1,5 @@
 import {
   useContext,
-  useEffect,
-  useState,
 } from "react";
 
 import {
@@ -9,132 +7,41 @@ import {
 } from "react-router-dom";
 
 import {
-  doc,
-  getDoc,
-} from "firebase/firestore";
-
-import {
   AuthContext,
 } from "../context/AuthContext";
-
-import {
-  db,
-} from "../firebase/firebase";
 
 function AdminRoute({
   children,
 }) {
 
-  const { currentUser } =
-    useContext(AuthContext);
+  const {
+    currentUser,
+  } = useContext(
+    AuthContext
+  );
 
-  const [loading, setLoading] =
-    useState(true);
-
-  const [isAdmin, setIsAdmin] =
-    useState(false);
-
-  // CHECK ADMIN ROLE
-  useEffect(() => {
-
-    const checkAdmin = async () => {
-
-      if (!currentUser) {
-
-        setLoading(false);
-
-        return;
-
-      }
-
-      try {
-
-        const userRef =
-          doc(
-            db,
-            "users",
-            currentUser.uid
-          );
-
-        const userSnap =
-          await getDoc(userRef);
-
-          console.log(
-  "CURRENT USER:",
-  currentUser
-);
-
-console.log(
-  "UID:",
-  currentUser?.uid
-);
-
-console.log(
-  "DOC EXISTS:",
-  userSnap.exists()
-);
-
-console.log(
-  "USER DATA:",
-  userSnap.data()
-);
-
-        if (
-          userSnap.exists()
-        ) {
-
-          const userData =
-            userSnap.data();
-
-          if (
-            userData.role ===
-            "admin"
-          ) {
-
-            setIsAdmin(true);
-
-          }
-
-        }
-
-      } catch (error) {
-
-        console.error(error);
-
-      } finally {
-
-        setLoading(false);
-
-      }
-
-    };
-
-    checkAdmin();
-
-  }, [currentUser]);
-
-  // LOADING
-  if (loading) {
+  // NOT LOGGED IN
+  if (!currentUser) {
 
     return (
-
-      <div className="min-h-screen flex items-center justify-center text-3xl font-bold text-green-600">
-
-        Checking Access...
-
-      </div>
-
+      <Navigate to="/login" />
     );
 
   }
 
   // NOT ADMIN
-  if (!isAdmin) {
+  if (
+    currentUser.role !==
+    "admin"
+  ) {
 
-    return <Navigate to="/" />;
+    return (
+      <Navigate to="/" />
+    );
 
   }
 
+  // ADMIN ACCESS
   return children;
 
 }
